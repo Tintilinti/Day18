c45aa3653381cd3d7f7316029669fca82f57b22c
Bruno Lovric <brunoolovric@gmail.com>
2021-03-26 02:50:55 +0100

    added iterable objects project

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+function* myGenerator() {
+    yield* [1, 2, 3, 4, 5];
+    yield* 'Arena';
+    yield* arguments;
+}
+let iterator = myGenerator(6, 7, 8);
+let generatorArray = [];
+for (i of iterator) {
+    generatorArray.push(i);
+}    
+console.log(generatorArray);
+module.exports = { generatorArray, myGenerator };
